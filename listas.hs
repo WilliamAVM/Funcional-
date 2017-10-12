5c3966347aba42c419d686a870112a7cf8f8ec52
Sergio Rodrigo <rodrigosergio93@gmail.com>
2017-10-12 10:50:50 -0400

    otra pratica sobre listas
    otras posibles soluciones subir al repo

--- listas.hs
+++ listas.hs
@@ -1,7 +1,10 @@
 {-
 	Ejercicios con listas
 -}
-l = [2, 4, 6, 7, 2, 4, 8, 7, 2]
+l1 = [2, 4, 6, 7, 2, 4, 8, 7, 2]
+l2 = [2, 4, 6, 8, 10, 12, 14]
+l3 = [4, 4, 4, 4, 4, 4, 4, 4]
+l4 = [[1, 2], [3, 4, 5], [6, 7, 8, 9], [10, 11]]
 
 -- 1. El seg elemnto
 segElem :: [a] -> a
@@ -26,3 +29,19 @@ iniMedFin xs = if (ini == med && ini == fin)then True else False
 		ini = head xs
 		med = (!!) xs ((div) (length xs) 2)
 		fin = last xs
+
+-- Otra practica -- FOTOGRAFIA
+
+-- 1. Devolver Verdad si todos los elementos son pares
+todosPares :: Integral a => [a] -> Bool
+todosPares xs = and (map even xs)
+
+-- 2. Todos los elementos iguales
+todosIguales :: Eq a => [a] -> Bool
+todosIguales xs = and (map f xs)
+	where f x = (head xs) == x
+
+-- 3. Recibe una lista de listas y devuelve una lista con las longitudes
+listaLongitud :: [[a]] -> [Int]
+listaLongitud xss = map f xss
+	where f xs = length xs 
