20db55084c8d41f8dd4ac39d16ae16620e2a973b
Sergio Rodrigo <rodrigosergio93@gmail.com>
2017-10-13 18:38:36 -0400

    Tipos de funciones
    agregue tipos de las funciones

--- foldr.hs
+++ foldr.hs
@@ -1,36 +1,44 @@
 
 l1 = [2, 3, 4, 5, 6, 7, 8] 
 l2 = [9,8,7,6, 5,4, 3, 2]
+
+miFilter :: (tx-> Bool) -> [tx] -> [tx]
 miFilter f xs = foldr g a xs
 	where
 		a = []
 		g x rs = if f x then x:rs else rs
 
+miMap :: (tx -> tf) -> [tx] -> [tf]
 miMap f xs = foldr g a xs
 	where
 		a = []
 		g x ls = (f x) : ls
 
+miReverse :: [ta] -> [ta]
 miReverse xs = foldr g a xs
 	where
 		a = []
 		g x ls = ls ++ [x]
 
+unir :: [tx] -> [tx] -> [tx]
 unir xs ys = foldr g a xs
 	where
 		a = ys
 		g x ls = x:ls
 
+miTakeWhile :: (tx -> Bool) -> [tx] -> [tx]
 miTakeWhile f xs = foldr g a xs
 	where
 		a = []
 		g x ls = if f x then x:ls else []
 
+mayor :: [Int] -> Int
 mayor xs = foldr g a xs
 	where
 		a = head xs
 		g x s = if x > s then x else s
 
+ordena :: [Int] -> [Int]
 ordena xs = foldr g a xs
 	where
 		a = []
